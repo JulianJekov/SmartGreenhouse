bc7c356c90f7d84f34c7e838aa6907dbfdda7f14
Julian Jekov <drmsz@abv.bg>
2025-08-05 18:49:30 +0300

    Greenhouse RestController implementation

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/GreenhouseController.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/GreenhouseController.java
@@ -1,7 +1,54 @@
 package com.smartgreenhouse.greenhouse.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.smartgreenhouse.greenhouse.dto.greenhouse.CreateGreenhouseDTO;
+import com.smartgreenhouse.greenhouse.dto.greenhouse.GreenhouseDTO;
+import com.smartgreenhouse.greenhouse.dto.greenhouse.UpdateGreenhouseDTO;
+import com.smartgreenhouse.greenhouse.service.GreenhouseService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/greenhouses")
 public class GreenhouseController {
+
+    private final GreenhouseService greenhouseService;
+
+    public GreenhouseController(GreenhouseService greenhouseService) {
+        this.greenhouseService = greenhouseService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GreenhouseDTO> getGreenhouseById(@PathVariable Long id){
+        GreenhouseDTO greenhouse = greenhouseService.getGreenhouseById(id);
+        return ResponseEntity.ok(greenhouse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GreenhouseDTO>> getAllGreenhouses(){
+        List<GreenhouseDTO> allGreenhouses = greenhouseService.getAllGreenhouses();
+        return ResponseEntity.ok(allGreenhouses);
+    }
+
+    @PostMapping
+    public ResponseEntity<GreenhouseDTO> createGreenhouse(@RequestBody @Valid CreateGreenhouseDTO  createGreenhouseDTO){
+        GreenhouseDTO createdGreenhouse = greenhouseService.createGreenhouse(createGreenhouseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdGreenhouse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<GreenhouseDTO> updateGreenhouse(@PathVariable Long id,
+                                                          @RequestBody @Valid UpdateGreenhouseDTO updateGreenhouseDTO){
+        GreenhouseDTO updatedGreenhouse = greenhouseService.updateGreenhouse(id, updateGreenhouseDTO);
+        return ResponseEntity.ok(updatedGreenhouse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GreenhouseDTO> deleteGreenhouse(@PathVariable Long id){
+        greenhouseService.deleteGreenhouse(id);
+        return ResponseEntity.noContent().build();
+    }
 }
