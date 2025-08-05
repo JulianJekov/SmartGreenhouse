d2dc884270bf9e79ae1540f86ccbf390191d87cc
Julian Jekov <drmsz@abv.bg>
2025-08-05 18:47:58 +0300

    GreenhouseService CRUD

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/GreenhouseService.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/GreenhouseService.java
@@ -1,5 +1,16 @@
 package com.smartgreenhouse.greenhouse.service;
 
 
+import com.smartgreenhouse.greenhouse.dto.greenhouse.CreateGreenhouseDTO;
+import com.smartgreenhouse.greenhouse.dto.greenhouse.GreenhouseDTO;
+import com.smartgreenhouse.greenhouse.dto.greenhouse.UpdateGreenhouseDTO;
+
+import java.util.List;
+
 public interface GreenhouseService {
+    GreenhouseDTO getGreenhouseById(Long id);
+    List<GreenhouseDTO> getAllGreenhouses();
+    GreenhouseDTO createGreenhouse(CreateGreenhouseDTO dto);
+    GreenhouseDTO updateGreenhouse(Long id, UpdateGreenhouseDTO dto);
+    void deleteGreenhouse(Long id);
 }
--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/impl/GreenhouseServiceImpl.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/impl/GreenhouseServiceImpl.java
@@ -1,8 +1,65 @@
 package com.smartgreenhouse.greenhouse.service.impl;
 
+import com.smartgreenhouse.greenhouse.dto.greenhouse.CreateGreenhouseDTO;
+import com.smartgreenhouse.greenhouse.dto.greenhouse.GreenhouseDTO;
+import com.smartgreenhouse.greenhouse.dto.greenhouse.UpdateGreenhouseDTO;
+import com.smartgreenhouse.greenhouse.entity.Greenhouse;
+import com.smartgreenhouse.greenhouse.repository.GreenhouseRepository;
 import com.smartgreenhouse.greenhouse.service.GreenhouseService;
+import com.smartgreenhouse.greenhouse.util.greenhouseMapper.GreenhouseMapper;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GreenhouseServiceImpl implements GreenhouseService {
+
+    private final GreenhouseRepository greenhouseRepository;
+    private final GreenhouseMapper greenhouseMapper;
+
+    public GreenhouseServiceImpl(GreenhouseRepository greenhouseRepository,
+                                 GreenhouseMapper greenhouseMapper) {
+        this.greenhouseRepository = greenhouseRepository;
+        this.greenhouseMapper = greenhouseMapper;
+    }
+
+    @Override
+    public GreenhouseDTO getGreenhouseById(Long id) {
+        Greenhouse greenhouse = greenhouseRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Greenhouse not found with ID: " + id));
+        return greenhouseMapper.toDto(greenhouse);
+    }
+
+    @Override
+    public List<GreenhouseDTO> getAllGreenhouses() {
+        return greenhouseRepository.findAll().stream()
+                .map(greenhouseMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public GreenhouseDTO createGreenhouse(CreateGreenhouseDTO dto) {
+        Greenhouse greenhouse = greenhouseMapper.toEntity(dto);
+        greenhouseRepository.save(greenhouse);
+        return greenhouseMapper.toDto(greenhouse);
+    }
+
+    @Override
+    public GreenhouseDTO updateGreenhouse(Long id, UpdateGreenhouseDTO dto) {
+        Greenhouse greenhouse = greenhouseRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Greenhouse not found with ID: " + id));
+        greenhouseMapper.updateEntity(dto,  greenhouse);
+        Greenhouse updated =  greenhouseRepository.save(greenhouse);
+        return greenhouseMapper.toDto(updated);
+    }
+
+    @Override
+    public void deleteGreenhouse(Long id) {
+        if (!greenhouseRepository.existsById(id)) {
+            throw new EntityNotFoundException("Greenhouse not found with ID: " + id);
+        }
+        greenhouseRepository.deleteById(id);
+    }
 }
