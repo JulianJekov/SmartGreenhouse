49add9735ccd97a8cf708cff3873b737fb6b55b9
Julian Jekov <drmsz@abv.bg>
2025-08-06 14:28:27 +0300

    Add SensorMapper

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/util/sensorMapper/SensorMapper.java
@@ -0,0 +1,45 @@
+package com.smartgreenhouse.greenhouse.util.sensorMapper;
+
+import com.smartgreenhouse.greenhouse.dto.sensor.CreateSensorDTO;
+import com.smartgreenhouse.greenhouse.dto.sensor.SensorDTO;
+import com.smartgreenhouse.greenhouse.dto.sensor.UpdateSensorDTO;
+import com.smartgreenhouse.greenhouse.entity.Greenhouse;
+import com.smartgreenhouse.greenhouse.entity.Sensor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SensorMapper {
+
+    public SensorDTO toDto(Sensor sensor){
+        SensorDTO dto = new SensorDTO();
+        dto.setId(sensor.getId());
+        dto.setType(sensor.getSensorType());
+        dto.setUnit(sensor.getUnit());
+        dto.setMinThreshold(sensor.getMinThreshold());
+        dto.setMaxThreshold(sensor.getMaxThreshold());
+        dto.setActive(sensor.getIsActive());
+        dto.setGreenhouseName(sensor.getGreenhouse().getName());
+        dto.setGreenhouseId(sensor.getGreenhouse().getId());
+        return dto;
+    }
+
+    public Sensor toEntity(CreateSensorDTO dto, Greenhouse greenhouse){
+        Sensor sensor = new Sensor();
+        sensor.setSensorType(dto.getSensorType());
+        sensor.setUnit(dto.getUnit());
+        sensor.setMinThreshold(dto.getMinThreshold());
+        sensor.setMaxThreshold(dto.getMaxThreshold());
+        sensor.setIsActive(dto.isActive());
+        sensor.setGreenhouse(greenhouse);
+        return sensor;
+    }
+
+    public void updateEntity(UpdateSensorDTO dto, Sensor sensor, Greenhouse greenhouse) {
+        sensor.setSensorType(dto.getType());
+        sensor.setUnit(dto.getUnit());
+        sensor.setMinThreshold(dto.getMinThreshold());
+        sensor.setMaxThreshold(dto.getMaxThreshold());
+        sensor.setIsActive(dto.isActive());
+        sensor.setGreenhouse(greenhouse); // If we want to change greenhouse later
+    }
+}
