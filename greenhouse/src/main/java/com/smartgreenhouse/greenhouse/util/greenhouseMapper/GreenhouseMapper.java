8472c529c14227ad766470a7aab31eb78c39c8d8
Julian Jekov <drmsz@abv.bg>
2025-08-06 12:43:01 +0300

    Update GreenhouseMapper.java

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/util/greenhouseMapper/GreenhouseMapper.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/util/greenhouseMapper/GreenhouseMapper.java
@@ -3,8 +3,12 @@ package com.smartgreenhouse.greenhouse.util.greenhouseMapper;
 import com.smartgreenhouse.greenhouse.dto.greenhouse.CreateGreenhouseDTO;
 import com.smartgreenhouse.greenhouse.dto.greenhouse.GreenhouseDTO;
 import com.smartgreenhouse.greenhouse.dto.greenhouse.UpdateGreenhouseDTO;
+import com.smartgreenhouse.greenhouse.dto.wateringLog.WateringLogDTO;
 import com.smartgreenhouse.greenhouse.entity.Greenhouse;
+import com.smartgreenhouse.greenhouse.entity.WateringLog;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class GreenhouseMapper {
@@ -13,6 +17,14 @@ public class GreenhouseMapper {
         dto.setId(greenhouse.getId());
         dto.setName(greenhouse.getName());
         dto.setLocation(greenhouse.getLocation());
+        dto.setCapacity(greenhouse.getCapacity());
+        if (greenhouse.getWateringLogs() != null){
+            List<WateringLogDTO> logs = greenhouse.getWateringLogs()
+                    .stream()
+                    .map(this::toWateringLogDto)
+                    .toList();
+            dto.setWateringLogs(logs);
+        }
         return dto;
     }
 
@@ -28,5 +40,13 @@ public class GreenhouseMapper {
         greenhouse.setName(dto.getName());
         greenhouse.setLocation(dto.getLocation());
         greenhouse.setCapacity(dto.getCapacity());
+    }
+
+    private WateringLogDTO toWateringLogDto(WateringLog wateringLog) {
+        WateringLogDTO dto = new WateringLogDTO();
+        dto.setId(wateringLog.getId());
+        dto.setTimestamp(wateringLog.getTimestamp());
+        dto.setAmount(wateringLog.getWaterAmount());
+        return dto;
     }
 }
