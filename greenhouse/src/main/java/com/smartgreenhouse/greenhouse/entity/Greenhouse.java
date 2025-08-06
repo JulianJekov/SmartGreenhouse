636e8fa35e5d1c628b4b94eb2148708271fccab2
Julian Jekov <drmsz@abv.bg>
2025-08-06 12:44:25 +0300

    Update greenhouse and greenhousedto

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/entity/Greenhouse.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/entity/Greenhouse.java
@@ -1,7 +1,10 @@
 package com.smartgreenhouse.greenhouse.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -10,6 +13,8 @@ import java.util.List;
 @Table
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Greenhouse {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -19,6 +24,9 @@ public class Greenhouse {
 
     private String location;
 
-    @OneToMany(mappedBy =  "greenhouse", cascade = CascadeType.ALL)
+    private Integer capacity;
+
+    @OneToMany(mappedBy =  "greenhouse", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private List<WateringLog> wateringLogs;
 }
