f6a093ac441449817fcf5741d64a567cf88bb06f
Julian Jekov <drmsz@abv.bg>
2025-08-07 11:38:54 +0300

    Update GlobalExceptionHandler.java

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/advice/GlobalExceptionHandler.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/advice/GlobalExceptionHandler.java
@@ -1,17 +1,42 @@
 package com.smartgreenhouse.greenhouse.controller.advice;
 
 import com.smartgreenhouse.greenhouse.exceptions.ObjectNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex){
+        LOGGER.error("An unexpected error occurred",ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred");
+    }
+
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(ObjectNotFoundException ex){
+    public ResponseEntity<String> handleEntityNotFoundException(ObjectNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 }
