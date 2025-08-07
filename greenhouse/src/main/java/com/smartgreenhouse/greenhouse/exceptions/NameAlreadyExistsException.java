1cf1f89528af5c30e678937d42e9f5baf5dcc904
Julian Jekov <drmsz@abv.bg>
2025-08-07 18:57:44 +0300

    Add ResponseStatus

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/exceptions/NameAlreadyExistsException.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/exceptions/NameAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.smartgreenhouse.greenhouse.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONTINUE)
 public class NameAlreadyExistsException extends RuntimeException {
     public NameAlreadyExistsException(String message) {
         super(message);
