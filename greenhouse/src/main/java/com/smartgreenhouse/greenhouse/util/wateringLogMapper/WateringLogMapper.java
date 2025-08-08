be1d399200e526d76647579a733ac1486999049f
Julian Jekov <drmsz@abv.bg>
2025-08-08 17:33:39 +0300

    Add WateringLogMapper

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/util/wateringLogMapper/WateringLogMapper.java
@@ -0,0 +1,31 @@
+package com.smartgreenhouse.greenhouse.util.wateringLogMapper;
+
+import com.smartgreenhouse.greenhouse.dto.wateringLog.CreateWateringLogDTO;
+import com.smartgreenhouse.greenhouse.dto.wateringLog.WateringLogDTO;
+import com.smartgreenhouse.greenhouse.entity.Greenhouse;
+import com.smartgreenhouse.greenhouse.entity.WateringLog;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class WateringLogMapper {
+
+    public WateringLogDTO toDto(WateringLog wateringLog) {
+        WateringLogDTO dto = new WateringLogDTO();
+        dto.setId(wateringLog.getId());
+        dto.setTimestamp(wateringLog.getTimestamp());
+        dto.setWaterAmount(wateringLog.getWaterAmount());
+        dto.setGreenhouseId(wateringLog.getGreenhouse().getId());
+        return dto;
+    }
+
+    public WateringLog toEntity(CreateWateringLogDTO dto, Greenhouse greenhouse) {
+        WateringLog wateringLog = new WateringLog();
+        wateringLog.setWaterAmount(dto.getWaterAmount());
+        wateringLog.setTimestamp(LocalDateTime.now());
+        wateringLog.setGreenhouse(greenhouse);
+        return wateringLog;
+    }
+
+}
