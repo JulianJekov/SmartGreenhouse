51eb8f02727d84964bb4bede66ae79776a763faa
Julian Jekov <drmsz@abv.bg>
2025-08-13 22:21:40 +0300

    Add new method in greenhouse repository
    Find greenhoses by autowatering enabled true

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/GreenhouseRepository.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/GreenhouseRepository.java
@@ -4,9 +4,15 @@ import com.smartgreenhouse.greenhouse.entity.Greenhouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface GreenhouseRepository extends JpaRepository<Greenhouse, Long> {
     boolean existsByNameAndIdNot(String name, Long id);
 
     boolean existsByName(String name);
+
+    List<Greenhouse> findByAutoWateringEnabledTrue();
+
 }
