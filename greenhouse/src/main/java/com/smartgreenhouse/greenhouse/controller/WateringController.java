1d8c5c7f632bb48977dc5813285d30908760bf23
Julian Jekov <drmsz@abv.bg>
2025-08-13 22:22:09 +0300

    Add Watering Controller
    Endpoint for manual watering

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/WateringController.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/WateringController.java
@@ -1,9 +1,30 @@
 package com.smartgreenhouse.greenhouse.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.smartgreenhouse.greenhouse.enums.WateringSource;
+import com.smartgreenhouse.greenhouse.exceptions.WateringFailedException;
+import com.smartgreenhouse.greenhouse.service.WateringService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/watering")
 public class WateringController {
+
+    private final WateringService wateringService;
+
+    public WateringController(WateringService wateringService) {
+        this.wateringService = wateringService;
+    }
+
+    @PostMapping("/manual")
+    public ResponseEntity<String> manualWatering(@RequestParam Long greenhouseId, @RequestParam Double amount) {
+        try {
+            wateringService.waterGreenhouse(greenhouseId, amount, WateringSource.MANUAL);
+            return ResponseEntity.ok().build();
+        } catch (WateringFailedException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+    }
 }
