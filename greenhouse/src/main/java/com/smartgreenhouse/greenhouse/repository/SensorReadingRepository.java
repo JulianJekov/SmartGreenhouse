746bfb1ef444bc9a1b913a94323baeb5f590075e
Julian Jekov <drmsz@abv.bg>
2025-08-20 22:23:38 +0300

    Add methods in SensorREadingRepository
    FindTopBySensorIdOrderByTimestampDesc
    FindAllBySensorIdAndTimestampBetweenOrderByTimestampAsc
    FindAllBySensorIdOrderByTimestampAsc

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/SensorReadingRepository.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/SensorReadingRepository.java
@@ -4,6 +4,17 @@ import com.smartgreenhouse.greenhouse.entity.SensorReading;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
+
+    Optional<SensorReading> findTopBySensorIdOrderByTimestampDesc(Long sensorId);
+
+    List<SensorReading> findAllBySensorIdAndTimestampBetweenOrderByTimestampAsc
+            (Long sensorId, LocalDateTime from, LocalDateTime to);
+
+    List<SensorReading> findAllBySensorIdOrderByTimestampAsc(Long sensorId);
 }
