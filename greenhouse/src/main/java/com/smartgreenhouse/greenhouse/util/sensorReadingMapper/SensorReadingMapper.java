40797ddd5f1a03b3020154faf51717b0aa11adf7
Julian Jekov <drmsz@abv.bg>
2025-08-20 22:24:35 +0300

    Add SensorReadingMapper

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/util/sensorReadingMapper/SensorReadingMapper.java
@@ -0,0 +1,40 @@
+package com.smartgreenhouse.greenhouse.util.sensorReadingMapper;
+
+import com.smartgreenhouse.greenhouse.dto.sensorReading.CreateSensorReadingDTO;
+import com.smartgreenhouse.greenhouse.dto.sensorReading.SensorReadingDTO;
+import com.smartgreenhouse.greenhouse.dto.sensorReading.TimeSeriesPointDTO;
+import com.smartgreenhouse.greenhouse.entity.Sensor;
+import com.smartgreenhouse.greenhouse.entity.SensorReading;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class SensorReadingMapper {
+
+    public SensorReadingDTO toDto(SensorReading sensorReading){
+        SensorReadingDTO sensorReadingDTO = new SensorReadingDTO();
+        sensorReadingDTO.setId(sensorReading.getId());
+        sensorReadingDTO.setSensorId(sensorReadingDTO.getSensorId());
+        sensorReadingDTO.setTimestamp(sensorReading.getTimestamp());
+        sensorReadingDTO.setValue(sensorReading.getValue());
+        sensorReadingDTO.setSensorUnit(sensorReading.getSensor().getUnit());
+        sensorReadingDTO.setSensorType(sensorReading.getSensor().getSensorType());
+        sensorReadingDTO.setGreenhouseId(sensorReading.getSensor().getGreenhouse().getId());
+        return sensorReadingDTO;
+    }
+
+    public SensorReading toEntity(CreateSensorReadingDTO createSensorReadingDTO, Sensor sensor) {
+        SensorReading sensorReading = new SensorReading();
+        sensorReading.setSensor(sensor);
+        sensorReading.setValue(createSensorReadingDTO.getValue());
+        return sensorReading;
+    }
+
+    public TimeSeriesPointDTO toTimeSeriesPointDTO(SensorReading sensorReading){
+        TimeSeriesPointDTO timeSeriesPointDTO = new TimeSeriesPointDTO();
+        timeSeriesPointDTO.setTimestamp(sensorReading.getTimestamp());
+        timeSeriesPointDTO.setValue(sensorReading.getValue());
+        return timeSeriesPointDTO;
+    }
+}
