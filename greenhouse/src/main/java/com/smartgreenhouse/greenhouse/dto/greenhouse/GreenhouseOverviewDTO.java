b28764a8602cc8435e93d31d2478748fb498f53d
Julian Jekov <drmsz@abv.bg>
2025-08-22 19:51:49 +0300

    Add GreenhouseOverviewDTO

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/dto/greenhouse/GreenhouseOverviewDTO.java
@@ -0,0 +1,20 @@
+package com.smartgreenhouse.greenhouse.dto.greenhouse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GreenhouseOverviewDTO {
+    private Long id;
+    private String name;
+    private Double currentTemperature;
+    private Double currentMoisture;
+    private Boolean autoWateringEnabled;
+    private Integer activeSensorCount;
+}
