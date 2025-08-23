678aae4cee6c1c9b4829dbedee1778161431439c
Julian Jekov <drmsz@abv.bg>
2025-08-23 20:01:43 +0300

    Add 2 custom queries in the SensorReadingRepository
    findLatestReadingsForAllSensors
    findLatestReadingsForSensors(with provided id-s)

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/SensorReadingRepository.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/SensorReadingRepository.java
@@ -2,6 +2,8 @@ package com.smartgreenhouse.greenhouse.repository;
 
 import com.smartgreenhouse.greenhouse.entity.SensorReading;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -19,4 +21,27 @@ public interface SensorReadingRepository extends JpaRepository<SensorReading, Lo
     List<SensorReading> findAllBySensorIdOrderByTimestampAsc(Long sensorId);
 
     List<SensorReading> findTop10BySensorIdOrderByTimestamp(Long sensorId);
+
+    @Query("""
+            SELECT r
+            FROM SensorReading r
+            WHERE r.timestamp = (
+            SELECT MAX(r2.timestamp)
+            FROM SensorReading r2
+            WHERE r2.sensor.id = r.sensor.id
+            )
+            """)
+    List<SensorReading> findLatestReadingsForAllSensors();
+
+    @Query("""
+                SELECT r
+                FROM SensorReading r
+                WHERE r.sensor.id IN :sensorIds
+                  AND r.timestamp = (
+                      SELECT MAX(r2.timestamp)
+                      FROM SensorReading r2
+                      WHERE r2.sensor.id = r.sensor.id
+                  )
+            """)
+    List<SensorReading> findLatestReadingsForSensors(@Param("sensorIds") List<Long> sensorIds);
 }
