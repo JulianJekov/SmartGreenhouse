4c9a31fd2a9d8194c452ea9a90c6f002197c387c
Julian Jekov <drmsz@abv.bg>
2025-08-25 22:36:28 +0300

    Add User Service and Repository

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.smartgreenhouse.greenhouse.repository;
+
+import com.smartgreenhouse.greenhouse.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.smartgreenhouse.greenhouse.service.impl;
+
+import com.smartgreenhouse.greenhouse.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
