e8bea693114cb56faf770cbca05c1d500a438026
Julian Jekov <drmsz@abv.bg>
2025-08-25 22:34:45 +0300

    Add User Entity

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/entity/User.java
@@ -0,0 +1,39 @@
+package com.smartgreenhouse.greenhouse.entity;
+
+import com.smartgreenhouse.greenhouse.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Greenhouse> greenhouses;
+}
