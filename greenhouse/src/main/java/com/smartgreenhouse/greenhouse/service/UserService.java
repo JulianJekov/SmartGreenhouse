2b8370d983fa3a189a91eb2021e3cd861bb3ccab
Julian Jekov <drmsz@abv.bg>
2025-08-27 21:36:05 +0300

    Add Register Login and Get Current User to the User Service

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/UserService.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/UserService.java
@@ -1,4 +1,13 @@
 package com.smartgreenhouse.greenhouse.service;
 
+import com.smartgreenhouse.greenhouse.dto.user.AuthResponse;
+import com.smartgreenhouse.greenhouse.dto.user.LoginRequest;
+import com.smartgreenhouse.greenhouse.dto.user.RegisterRequest;
+import com.smartgreenhouse.greenhouse.dto.user.UserDTO;
+import com.smartgreenhouse.greenhouse.entity.User;
+
 public interface UserService {
+    AuthResponse register(RegisterRequest registerRequest);
+    AuthResponse login(LoginRequest loginRequest);
+    UserDTO getCurrentUser(String email);
 }
--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/impl/UserServiceImpl.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/impl/UserServiceImpl.java
@@ -1,8 +1,73 @@
 package com.smartgreenhouse.greenhouse.service.impl;
 
+import com.smartgreenhouse.greenhouse.dto.user.AuthResponse;
+import com.smartgreenhouse.greenhouse.dto.user.LoginRequest;
+import com.smartgreenhouse.greenhouse.dto.user.RegisterRequest;
+import com.smartgreenhouse.greenhouse.dto.user.UserDTO;
+import com.smartgreenhouse.greenhouse.entity.User;
+import com.smartgreenhouse.greenhouse.exceptions.EmailAlreadyExistsException;
+import com.smartgreenhouse.greenhouse.exceptions.ObjectNotFoundException;
+import com.smartgreenhouse.greenhouse.jwt.JWTHelper;
+import com.smartgreenhouse.greenhouse.repository.UserRepository;
 import com.smartgreenhouse.greenhouse.service.UserService;
+import com.smartgreenhouse.greenhouse.util.userMapper.UserMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTHelper jWTHelper;
+
+    public UserServiceImpl(UserRepository userRepository,
+                           UserMapper userMapper,
+                           PasswordEncoder passwordEncoder, JWTHelper jWTHelper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+        this.jWTHelper = jWTHelper;
+    }
+
+    @Override
+    public AuthResponse register(RegisterRequest registerRequest) {
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            throw new EmailAlreadyExistsException("Email already exists");
+        }
+
+        User user = userMapper.toEntity(registerRequest);
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        User savedUser = userRepository.save(user);
+
+        String token = jWTHelper.generateToken(savedUser.getEmail(), savedUser.getRole().name());
+
+        AuthResponse authResponse = userMapper.toAuthResponse(savedUser);
+        authResponse.setToken(token);
+
+        return authResponse;
+    }
+
+    @Override
+    public AuthResponse login(LoginRequest loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid credentials"));
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("Invalid credentials");
+        }
+        String token = jWTHelper.generateToken(user.getEmail(), user.getRole().name());
+        AuthResponse authResponse = userMapper.toAuthResponse(user);
+        authResponse.setToken(token);
+        return authResponse;
+    }
+
+    @Override
+    public UserDTO getCurrentUser(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ObjectNotFoundException("User not found"));
+        return userMapper.toDTO(user);
+    }
+
+
 }
