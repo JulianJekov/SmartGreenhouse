859c861c49f9fa4aef071f5a0dcc5239058cc946
Julian Jekov <drmsz@abv.bg>
2025-08-30 20:04:21 +0300

    Create Refresh Token Service Impl

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/RefreshTokenServiceImpl.java
@@ -0,0 +1,70 @@
+package com.smartgreenhouse.greenhouse.service;
+
+import com.smartgreenhouse.greenhouse.entity.RefreshToken;
+import com.smartgreenhouse.greenhouse.entity.User;
+import com.smartgreenhouse.greenhouse.exceptions.ObjectNotFoundException;
+import com.smartgreenhouse.greenhouse.exceptions.TokenException;
+import com.smartgreenhouse.greenhouse.repository.RefreshTokenRepository;
+import com.smartgreenhouse.greenhouse.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshTokenExpirationMs;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+
+    public RefreshTokenServiceImpl(RefreshTokenRepository refreshTokenRepository, UserRepository userRepository) {
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public RefreshToken generateRefreshToken(String userEmail) {
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new ObjectNotFoundException("User with email" + userEmail + "not found"));
+
+        refreshTokenRepository.revokeAllUserTokens(user.getId());
+
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUser(user);
+        refreshToken.setExpiryDate(Instant.now().plusMillis(refreshTokenExpirationMs));
+        refreshToken.setToken(UUID.randomUUID().toString());
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    @Override
+    public RefreshToken verifyRefreshToken(String token) {
+        return refreshTokenRepository.findByToken(token)
+                .filter(refreshToken -> !isExpired(refreshToken))
+                .filter(refreshToken -> !isRevoked(refreshToken))
+                .orElseThrow(() -> new TokenException("Invalid refresh token"));
+    }
+
+    @Override
+    public void revokeRefreshToken(String token) {
+        refreshTokenRepository.findByToken(token)
+                .ifPresent(refreshToken -> {
+                    refreshToken.setRevoked(true);
+                    refreshTokenRepository.save(refreshToken);
+                });
+    }
+
+    private boolean isRevoked(RefreshToken refreshToken) {
+        return Boolean.TRUE.equals(refreshToken.getRevoked());
+    }
+
+    private boolean isExpired(RefreshToken refreshToken) {
+        return Instant.now().isAfter(refreshToken.getExpiryDate());
+    }
+
+
+}
