419228b6eb56202ff863587f1927da7dd6494714
Julian Jekov <drmsz@abv.bg>
2025-08-30 19:22:37 +0300

    Create Refresh Token Entity

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/entity/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.smartgreenhouse.greenhouse.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+    
+    private Boolean revoked = false;
+}
