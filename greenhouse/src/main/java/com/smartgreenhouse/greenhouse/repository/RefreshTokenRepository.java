0f59faa4dc79be1eafde081677dc4a859ee24dbf
Julian Jekov <drmsz@abv.bg>
2025-08-31 17:08:54 +0300

    Add @Transactional to revokeAllUserTokens method
    Annotated the revokeAllUserTokens method with @Transactional to ensure proper transaction management during token revocation.

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/RefreshTokenRepository.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/RefreshTokenRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     @Modifying
     @Query("UPDATE RefreshToken rt SET rt.revoked = true WHERE rt.user.id = :userId AND rt.revoked = false ")
+    @Transactional
     void revokeAllUserTokens(Long userId);
 
     Optional<RefreshToken> findByToken(String token);
