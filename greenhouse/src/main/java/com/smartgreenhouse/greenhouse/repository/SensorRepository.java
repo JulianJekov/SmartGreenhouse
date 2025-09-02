fd152bccba520fe02ba45a7c750fa828862b9b51
Julian Jekov <drmsz@abv.bg>
2025-09-02 20:56:48 +0300

    Add user email-based sensor queries to repository
    Added methods to SensorRepository for finding sensors by greenhouse user email, checking existence by sensor ID and user email, and retrieving sensors by ID and user email. These changes support more granular access control and querying based on user context.

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/SensorRepository.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/SensorRepository.java
@@ -5,8 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor,Long> {
     List<Sensor> findByGreenhouseId(Long greenhouseId);
+
+    Optional<Sensor> findByIdAndGreenhouseUserEmail(Long id, String greenhouseUserEmail);
+
+    List<Sensor> findAllByGreenhouseUserEmail(String email);
+
+    boolean existsByIdAndGreenhouseUserEmail(Long id, String email);
 }
