7d7746fac2dc66a8ea9fc2e342c7385676071927
Julian Jekov <drmsz@abv.bg>
2025-09-03 22:30:38 +0300

    Add email parameter to WateringLogService methods
    Updated WateringLogService interface to require an email parameter for creating and retrieving watering logs, enabling user-specific operations.

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/WateringLogService.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/WateringLogService.java
@@ -5,11 +5,11 @@ import com.smartgreenhouse.greenhouse.dto.wateringLog.WateringLogDTO;
 import org.springframework.data.domain.Page;
 
 public interface WateringLogService {
-    WateringLogDTO createWateringLog(CreateWateringLogDTO dto);
+    WateringLogDTO createWateringLog(CreateWateringLogDTO dto, String email);
 
-    Page<WateringLogDTO> getWateringLogByGreenhouseId(Long id, int page, int size);
+    Page<WateringLogDTO> getWateringLogByGreenhouseId(Long id, String email, int page, int size);
 
-    Page<WateringLogDTO> getAllWateringLogs(int page, int size);
+    Page<WateringLogDTO> getAllWateringLogs(String email, int page, int size);
 
 
 }
