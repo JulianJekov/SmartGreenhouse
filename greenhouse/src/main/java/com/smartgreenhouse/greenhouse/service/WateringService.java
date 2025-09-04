09ff5a0ee5e6b3d1ed43eac9b380e0e648d51ff8
Julian Jekov <drmsz@abv.bg>
2025-09-04 19:16:04 +0300

    Add email parameter to waterGreenhouse method
    The waterGreenhouse method in WateringService now requires an email parameter, to associate watering actions with a user.

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/WateringService.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/WateringService.java
@@ -3,5 +3,5 @@ package com.smartgreenhouse.greenhouse.service;
 import com.smartgreenhouse.greenhouse.enums.WateringSource;
 
 public interface WateringService {
-    void waterGreenhouse(Long greenhouseId, Double amount, WateringSource wateringSource);
+    void waterGreenhouse(Long greenhouseId, String email, Double amount, WateringSource wateringSource);
 }
