a435acc54530a314095035403f22e862e4b2588e
Julian Jekov <drmsz@abv.bg>
2025-09-05 19:31:27 +0300

    Allow public access to Swagger UI and API docs
    Added permitAll() for '/swagger-ui/**' and '/v3/api-docs/**' endpoints in SecurityConfig to enable public access to API documentation resources.

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/config/SecurityConfig.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/user/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
