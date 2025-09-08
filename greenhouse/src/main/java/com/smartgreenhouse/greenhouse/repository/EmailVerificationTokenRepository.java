81026238e1408130025de965051ea9b76c8462ae
Julian Jekov <drmsz@abv.bg>
2025-09-08 20:04:03 +0300

    Add revokeAllUserTokens method to the EmailVerificationTokenRepository
    This query revokes all email verification tokens fro a given user ID

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/EmailVerificationTokenRepository.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/EmailVerificationTokenRepository.java
@@ -2,6 +2,9 @@ package com.smartgreenhouse.greenhouse.repository;
 
 import com.smartgreenhouse.greenhouse.entity.EmailVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,8 @@ import java.util.Optional;
 @Repository
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
     Optional<EmailVerificationToken> findByToken(String token);
+
+    @Modifying
+    @Query("UPDATE EmailVerificationToken t SET t.revoked = true WHERE t.user.id = :userId")
+    void revokeAllUserTokens(@Param("userId") Long id);
 }
