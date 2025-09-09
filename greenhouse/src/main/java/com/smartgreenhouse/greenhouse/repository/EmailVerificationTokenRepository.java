3bc82eae693b8ae572cad3e18d1733ae83396d52
Julian Jekov <drmsz@abv.bg>
2025-09-09 12:12:55 +0300

    Update EmailVerificationTokenRepository.java
    Add custom query to count all tokens created in the last hour

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/EmailVerificationTokenRepository.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/repository/EmailVerificationTokenRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +17,7 @@ public interface EmailVerificationTokenRepository extends JpaRepository<EmailVer
     @Modifying
     @Query("UPDATE EmailVerificationToken t SET t.revoked = true WHERE t.user.id = :userId")
     void revokeAllUserTokens(@Param("userId") Long id);
+
+    @Query("SELECT COUNT (t) FROM EmailVerificationToken t WHERE t.user.id = :userId AND t.created > :oneHourAgo")
+    long countRecentTokensByUserId(@Param("userId") Long userId, @Param("oneHourAgo") Instant oneHourAgo);
 }
