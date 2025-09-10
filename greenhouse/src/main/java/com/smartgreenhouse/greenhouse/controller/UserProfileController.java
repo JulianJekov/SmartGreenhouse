6b891aed726e2bffd144037b43c5a2fe5374e56c
Julian Jekov <drmsz@abv.bg>
2025-09-10 12:14:14 +0300

    Update UserProfileController.java

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/UserProfileController.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/controller/UserProfileController.java
@@ -1,3 +1,27 @@
+package com.smartgreenhouse.greenhouse.controller;
+
+import com.smartgreenhouse.greenhouse.dto.user.UserDTO;
+import com.smartgreenhouse.greenhouse.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user/profile")
+public class UserProfileController {
+
+    private final UserService userService;
+
+    public UserProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<UserDTO> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
+        UserDTO currentUser = userService.getCurrentUser(userDetails.getUsername());
+        return ResponseEntity.ok(currentUser);
+    }
 //
 //    @PutMapping
 //    public UserDTO updateProfile(@AuthenticationPrincipal UserDetails userDetails,
