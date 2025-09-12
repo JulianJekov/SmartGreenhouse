52dcd28dd48969dc64d0873bc98d16908ae72806
Julian Jekov <drmsz@abv.bg>
2025-09-12 16:18:24 +0300

    Add change password method
    Get the useer
    Check if old password from the dto is equal to user current password if not throw
    Check if new password is the same as current password if yes throw
    Update the password with new one and save the user
    revoke all user tokens

--- greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/impl/UserServiceImpl.java
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/service/impl/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package com.smartgreenhouse.greenhouse.service.impl;
 
+import com.smartgreenhouse.greenhouse.dto.user.ChangePasswordRequest;
 import com.smartgreenhouse.greenhouse.dto.user.UserDTO;
 import com.smartgreenhouse.greenhouse.entity.User;
+import com.smartgreenhouse.greenhouse.exceptions.InvalidPasswordException;
 import com.smartgreenhouse.greenhouse.exceptions.ObjectNotFoundException;
+import com.smartgreenhouse.greenhouse.repository.RefreshTokenRepository;
 import com.smartgreenhouse.greenhouse.repository.UserRepository;
 import com.smartgreenhouse.greenhouse.service.UserService;
 import com.smartgreenhouse.greenhouse.util.userMapper.UserMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +18,16 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     public UserServiceImpl(UserRepository userRepository,
-                           UserMapper userMapper) {
+                           UserMapper userMapper,
+                           PasswordEncoder passwordEncoder, RefreshTokenRepository refreshTokenRepository) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     @Transactional(readOnly = true)
@@ -32,6 +41,23 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUserByEmail(String email) {
         return getUser(email);
+    }
+
+    @Override
+    public void changePassword(String email, ChangePasswordRequest request) {
+        User user = getUser(email);
+
+        if (!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
+            throw new InvalidPasswordException("Current password is incorrect");
+        }
+
+        if (request.getNewPassword().equals(request.getOldPassword())) {
+            throw new InvalidPasswordException("New password must be different from current password");
+        }
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
+
+        refreshTokenRepository.revokeAllUserTokens(user.getId());
     }
 
     private User getUser(String email) {
