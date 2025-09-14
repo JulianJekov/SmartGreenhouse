006765eef4869cf51463e053af6ca6de81257371
Julian Jekov <drmsz@abv.bg>
2025-09-14 21:07:09 +0300

    Create ErrorResponse.java

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/dto/ErrorResponse.java
@@ -0,0 +1,25 @@
+package com.smartgreenhouse.greenhouse.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
+    }
+}
