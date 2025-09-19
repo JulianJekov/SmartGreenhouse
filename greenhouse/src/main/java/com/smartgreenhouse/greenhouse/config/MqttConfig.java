eec76a067d252b99dc7fa75db29066b0785cd8fe
Julian Jekov <drmsz@abv.bg>
2025-09-19 18:32:00 +0300

    Add MQTT configuration
    Configurate MqttClient with connection options

---
+++ greenhouse/src/main/java/com/smartgreenhouse/greenhouse/config/MqttConfig.java
@@ -0,0 +1,35 @@
+package com.smartgreenhouse.greenhouse.config;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqttConfig {
+
+    @Value("${mqtt.broker-url}")
+    private String brokerUrl;
+
+    @Value("${mqtt.client-id}")
+    private String clientId;
+
+    @Bean
+    public MqttClient mqttClient(MqttConnectOptions options) throws MqttException {
+        MqttClient client = new MqttClient(brokerUrl, clientId);
+        client.connect(options);
+        return client;
+    }
+
+    @Bean
+    public MqttConnectOptions mqttConnectOptions() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setServerURIs(new String[]{brokerUrl});
+        options.setCleanSession(true);
+        options.setAutomaticReconnect(true);
+        options.setConnectionTimeout(10);
+        return options;
+    }
+}
